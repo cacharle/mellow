fd768eccfc37b6f2abaedc632e47588f849e4934
Charles Cabergs <me@cacharle.xyz>
2022-01-19 08:18:09 +0100

    Added test for 2d array alocation and alignment test

--- test/test_malloc.c
+++ test/test_malloc.c
@@ -27,7 +27,13 @@ assert_heap_eq(heap_layout_t heap_layout, size_t heap_layout_len)
     {
         if (heap_layout[i].payload_size != -1)
         {
-            cr_assert_eq(block_payload_size(curr), heap_layout[i].payload_size);
+            cr_assert_eq(
+                block_payload_size(curr),
+                heap_layout[i].payload_size,
+                "block payload %zu != %zu",
+                block_payload_size(curr),
+                heap_layout[i].payload_size
+            );
             cr_assert_eq(block_size(curr), heap_layout[i].payload_size + BLOCK_METADATA_SIZE);
         }
         if (heap_layout[i].state == AVAILABLE)
@@ -64,19 +70,60 @@ Test(mw_malloc, basic)
     ASSERT_HEAP_EQ(heap_layout);
 }
 
-// Test(mw_malloc, array_2d)
-// {
-//     size_t len = 10;
-//     size_t row_len = 10;
-//     int **p = mw_malloc(sizeof(int*) * len);
-//     for (int i = 0; i < len; i++)
-//         p[i] = mw_malloc(sizeof(int) * row_len);
-//
-//     for (int i = 0; i < len; i++)
-//         for (int j = 0; j < row_len; j++)
-//             p[i][j] = i * j;
-//     // check debug
-// }
+Test(mw_malloc, array_2d)
+{
+    size_t len = 4;
+    size_t payload_size = sizeof(int*) * len;
+    size_t row_payload_size = sizeof(int) * len;
+    int **p = mw_malloc(payload_size);
+    for (int i = 0; i < len; i++)
+        p[i] = mw_malloc(row_payload_size);
+    for (int i = 0; i < len; i++)
+        for (int j = 0; j < len; j++)
+            p[i][j] = i * j;
+    heap_layout_t heap_layout = {
+        {OCCUPIED,  .payload_size = payload_size, .payload = p},
+        {OCCUPIED,  .payload_size = row_payload_size, .payload = p[0]},
+        {OCCUPIED,  .payload_size = row_payload_size, .payload = p[1]},
+        {OCCUPIED,  .payload_size = row_payload_size, .payload = p[2]},
+        {OCCUPIED,  .payload_size = row_payload_size, .payload = p[3]},
+        {AVAILABLE, .payload_size = -1, .payload = NULL}
+    };
+    ASSERT_HEAP_EQ(heap_layout);
+}
+
+Test(mw_malloc, alignment_8)
+{
+    size_t size = 7;
+    mw_malloc(size);
+    heap_layout_t heap_layout = {
+        {OCCUPIED,  .payload_size = 8, .payload = NULL},
+        {AVAILABLE, .payload_size = -1, .payload = NULL}
+    };
+    ASSERT_HEAP_EQ(heap_layout);
+}
+
+Test(mw_malloc, alignment_16)
+{
+    size_t size = 10;
+    mw_malloc(size);
+    heap_layout_t heap_layout = {
+        {OCCUPIED,  .payload_size = 16, .payload = NULL},
+        {AVAILABLE, .payload_size = -1, .payload = NULL}
+    };
+    ASSERT_HEAP_EQ(heap_layout);
+}
+
+Test(mw_malloc, alignment_32)
+{
+    size_t size = 25;
+    mw_malloc(size);
+    heap_layout_t heap_layout = {
+        {OCCUPIED,  .payload_size = 32, .payload = NULL},
+        {AVAILABLE, .payload_size = -1, .payload = NULL}
+    };
+    ASSERT_HEAP_EQ(heap_layout);
+}
 
 // Test(mw_free, basic)
 // {
